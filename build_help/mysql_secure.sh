dd1073d1b4ab08fed917f869c6f53b82c22da70d
Nathan Thorpe <nth0rpe@uw.edu>
2017-05-28 15:48:49 -0700

    mysql config

--- build_help/mysql_secure.sh
+++ build_help/mysql_secure.sh
@@ -4,3 +4,4 @@ mysqladmin -u root password linux
 mysql -u root -plinux -e "CREATE USER 'oai'@'127.0.0.1' IDENTIFIED BY 'linux';"
 mysql -u root -plinux -e "GRANT ALL PRIVILEGES ON oai_db.* to 'oai'@'127.0.0.1';"
 mysql -u root -plinux -e "FLUSH PRIVILEGES;"
+mysql -u root -plinux oai_db < oai.db.sql
