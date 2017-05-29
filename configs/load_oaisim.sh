6c39d25c52da96963d06205fe6fe934b90acecd0
Nathan Thorpe <nth0rpe@uw.edu>
2017-05-28 21:30:33 -0700

    make separate configs for oaisim and enb+ue on usrp

---
+++ configs/load_oaisim.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Run build script before
+sudo cp mme_oaisim.conf     /usr/local/etc/oai/mme.conf
+sudo cp hss.conf            /usr/local/etc/oai/hss.conf
+sudo cp spgw_oaisim.conf    /usr/local/etc/oai/spgw.conf
+
---
+++ configs/load_usrp.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Run build script before
+sudo cp mme.conf     /usr/local/etc/oai/mme.conf
+sudo cp hss.conf     /usr/local/etc/oai/hss.conf
+sudo cp spgw.conf    /usr/local/etc/oai/spgw.conf
+
